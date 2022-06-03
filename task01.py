9f6733a7d9793de0169cde453638a16b4efa4999
Dmitry Mordanov <mordanov314@gmail.com>
2022-06-03 13:31:10 +0300

    first commit

---
+++ task01.py
@@ -0,0 +1,10 @@
+ # Напишите программу, удаляющую из текста все слова содержащие "абв". 
+ # Используйте знания с последней лекции. Выполните ее в виде функции. 
+
+
+ git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Zep314/Py_HomeWork05.git
+git push -u origin main
